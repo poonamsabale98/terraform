bd500d8eabeaf4922783047b2b4d63daae5edcde
POONAM <poonamsabale413709@gmail.com>
2024-11-21 17:37:53 +0530

    qwer

--- Moduleblock/Dev/main.tf
+++ Moduleblock/Dev/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source               = "/home/cloudshell-user/Terraform/ModuleBlock/Resources/VPC"
+  source               = "/home/cloudshell-user/terraform/Moduleblock/Resources/VPC"
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidr   = "10.0.1.0/24"
   private_subnet_cidr  = "10.0.2.0/24"
@@ -13,7 +13,7 @@ module "vpc" {
 }
 
 module "ec2" {
-  source          = "/home/cloudshell-user/Terraform/ModuleBlock/Resources/EC2"
+  source          = "/home/cloudshell-user/terraform/Moduleblock/Resources/EC2"
   vpc_id          = module.vpc.vpc_id
   subnet_id       = module.vpc.public_subnet_id
   ami_id          = "ami-047126e50991d067b" # Replace with a valid AMI ID
