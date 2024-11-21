4e015d384565d02b355ae6c34632e613b224f8d7
POONAM <poonamsabale413709@gmail.com>
2024-11-21 11:26:31 +0530

    av

--- outputblock/ec2main.tf
+++ outputblock/ec2main.tf
@@ -26,7 +26,7 @@ resource "aws_vpc" "this_vpc" {
 
 resource "aws_subnet" "this_subnet_pub" {
   vpc_id     = aws_vpc.this_vpc.id  #referedresourceblock'sProvider_referedresourceblock'sresourcetype.referedresourceblocksuniqueresourceblockname.attributeofresource
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-southeast-1"
   cidr_block = var.this_subnet_pub_cidr_block      //"12.11.0.0/17"  
   map_public_ip_on_launch = var.this_subnet_pub_map_ip //true
   tags = {
