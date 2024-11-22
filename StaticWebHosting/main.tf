f70932081bde27ed4018181360d6a1bf7c78e9e9
POONAM <poonamsabale413709@gmail.com>
2024-11-22 13:10:38 +0530

    asdfgh

--- StaticWebHosting/main.tf
+++ StaticWebHosting/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "boxer_sg" {
 resource "aws_instance" "boxer_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  security_groups = [aws_security_group.boxer_sg.name]
+  vpc_security_group_ids = [aws_security_group.boxer_sg.name]
   key_name      = var.key_name
 
   # User data script to install Nginx and set up the boxer app
